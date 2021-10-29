e7a00142f6adaa7fe8d07ad5fdd7ed5c847580d6
SiniJohanna <82939474+SiniJohanna@users.noreply.github.com>
2021-10-29 16:56:42 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,56 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
+
+export default function App() {
+
+  const [text, setText] = useState('');
+  const [data, setData] = useState([]);
+
+  const buttonPressed = () => {
+    setData([...data, { key: text }]);
+    setText('');
+  }
+const buttonClear = () => {
+  setData([]);
+  setText('');
+}
+
+  return (
+    <View style={styles.container}>
+      <TextInput style={styles.input} onChangeText={text => setText(text)} value={text} />
+      <View style={{flexDirection:'row'}}>
+      <Button onPress={buttonPressed} title="Add" />
+      <Button onPress={buttonClear} title="Clear" />
+      </View>
+      <FlatList 
+        ListHeaderComponent= {<Text style={styles.listHeader}>Shopping list</Text>}
+        data={data}
+        renderItem={({ item }) =>
+          <Text>{item.key}</Text>
+        }
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  input: {
+    marginTop: 50,
+    marginBottom: 5,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1 
+  },
+  listHeader: {
+    color:'blue',
+    fontWeight: 'bold',
+  }
+});
